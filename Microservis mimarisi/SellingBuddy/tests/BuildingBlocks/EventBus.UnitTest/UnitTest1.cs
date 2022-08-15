4e6d8ce1116cfd7ec5ce40f78f407b60ef8b4975
gulsaherturk <gulsaherturk58@gmail.com>
2022-08-15 17:44:06 +0300

    15,08,22

---
+++ Microservis mimarisi/SellingBuddy/tests/BuildingBlocks/EventBus.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace EventBus.UnitTest
+{
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+
+        }
+    }
+}
