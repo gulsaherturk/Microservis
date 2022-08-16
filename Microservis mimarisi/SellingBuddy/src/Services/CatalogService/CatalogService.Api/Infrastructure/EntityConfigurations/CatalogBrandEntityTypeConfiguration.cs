00c10ef1e195ea892e7f1a609b81f88a168c92bd
gulsaherturk <gulsaherturk58@gmail.com>
2022-08-16 17:38:13 +0300

    16.08.22

---
+++ Microservis mimarisi/SellingBuddy/src/Services/CatalogService/CatalogService.Api/Infrastructure/EntityConfigurations/CatalogBrandEntityTypeConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CatalogService.Api.Infrastructure.Context;
+using CatalogService.Api.Core.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CatalogService.Api.Infrastructure.EntityConfigurations
+{
+    public class CatalogBrandEntityTypeConfiguration:IEntityTypeConfiguration<CatalogBrand>
+
+    {
+       
+public void Configure(EntityTypeBuilder<CatalogBrand> builder)
+        {
+           builder.ToTable("CatalogBrand",CatalogContext.DEFAULT_SCHEMA);
+            builder.HasKey(ci=>ci.Id);
+            builder.Property(ci => ci.Id)
+                .UseHiLo("catalog_brand_hilo")
+                .IsRequired();
+
+            builder.Property(cb => cb.Brand)
+                .IsRequired()
+                .HasMaxLength(100);
+
+        }
+    }
+
+}
